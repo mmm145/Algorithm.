db77d52b63f2d9e02cfc571aefd0899c2e387f21
Masahiro <55323414+mmm145@users.noreply.github.com>
2021-10-10 19:18:10 +0900

    Add files via upload

---
+++ graph/bfs.cpp
@@ -0,0 +1,60 @@
+﻿#include<iostream>
+#include<queue>
+#define max 200
+#define white 0
+#define gray 1
+#define black 2
+using namespace std;
+int M[max][max];
+int d[max];
+int state[max];
+int main() {
+	/* 
+	 input ex)
+	 4
+	 1 2 2 4
+	 2 1 4
+	 3 0
+	 4 1 3
+	 */
+	int n;// the number of vertex
+	cin >> n;
+
+	// initialization 
+	for (int i = 0; i < n; i++)for (int j = 0; j < n; j++)M[i][j] = 0;
+	for (int i = 0; i < n; i++) {
+		d[i] = -1;
+		state[i] = white;
+	}
+	//storing the information of graph into M[][]
+	for (int i = 0; i < n; i++) {
+		int u, k;
+		cin >> u >> k;
+		for (int j = 0; j < k; j++) {
+			int v;
+			cin >> v;
+			M[u - 1][v - 1] = 1;
+		}
+	}
+	queue<int> que;
+	que.push(0);
+	d[0] = 0;
+	state[0] = gray;
+	while (!que.empty()) {
+		int current = que.front();
+		que.pop();
+		state[current] = black;
+		for (int i = 0; i < n; i++) {
+			if (!M[current][i])continue;
+			if (state[i])continue;
+			d[i] = d[current] + 1;
+			state[i] = gray;
+			que.push(i);
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		cout << i + 1 << " " << d[i] << endl;
+	}
+	// output is the shortest distance between each node and node 1.
+	return 0;
+}
